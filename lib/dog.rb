d8c004ef6968e55e14ea0f94707014d0710a4e71
Ryan McCahon <mccahonr@yahoo.com>
2019-08-19 23:48:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 # Your code goes here!
 class Dog
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+  def name=(name)
+    @name = name
   end
   def name
-    @this_dogs_name
+    @name    
   end
   def bark
     puts "woof!"
